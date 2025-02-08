50bee5a132c85bc311224efe4e0a29deb84d1fc4
kkeizzah <keizzahketh@gmail.com>
2025-02-08 16:56:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* this is the main file */
-
 const {
   default: KeithConnect,
   useMultiFileAuthState,
@@ -15,44 +13,31 @@ const pino = require("pino");
 const { Boom } = require("@hapi/boom");
 const fs = require("fs");
 const FileType = require("file-type");
-const { exec, spawn, execSync } = require("child_process");
 const axios = require("axios");
 const chalk = require("chalk");
-const figlet = require("figlet");
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 10000;
-const _ = require("lodash");
-const PhoneNumber = require("awesome-phonenumber");
+const { DateTime } = require("luxon");
+
+const { smsg } = require('./smsg');
+const authenticationn = require('./auth.js');
+const { autoview, autoread, botname, autobio, mode, prefix, autoreact, presence, autolike, anticall } = require('./settings');
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif');
-const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/botFunctions');
+const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, sleep } = require('./lib/botFunctions');
 const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
 
-const authenticationn = require('./auth.js');
-const { smsg } = require('./smsg');
+const app = express();
+const port = process.env.PORT || 10000;
 
-const { autoview, autoread, botname, autobio, mode, prefix, autoreact, presence, autolike, anticall } = require('./settings');
-const { DateTime } = require('luxon');
-const { commands, totalCommands } = require('./commandHandler');
 authenticationn();
 const groupEvents = require("./groupEvents.js");
-// const connectionEvents = require("./connectionEvents.js");
 
 async function startKeith() {
-
-  const { saveCreds, state } = await useMultiFileAuthState(`session`)
+  const { saveCreds, state } = await useMultiFileAuthState(`session`);
   const client = KeithConnect({
     logger: pino({ level: 'silent' }),
     printQRInTerminal: true,
     version: [2, 3000, 1015901307],
     browser: [`KEITH-MD`, 'Safari', '3.0'],
-    fireInitQueries: false,
-    shouldSyncHistoryMessage: true,
-    downloadHistory: true,
-    syncFullHistory: true,
-    generateHighQualityLinkPreview: true,
-    markOnlineOnConnect: true,
-    keepAliveIntervalMs: 30000,
     auth: state,
     getMessage: async (key) => {
       if (store) {
@@ -62,72 +47,62 @@ async function startKeith() {
       return { conversation: "HERE" };
     }
   });
-  
- const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// Track the last text time to prevent overflow
-let lastTextTime = 0;
-const messageDelay = 5000; // Set the minimum delay between messages (in milliseconds)
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-client.ev.on('call', async (callData) => {
-  if (anticall === 'true') {
-    const callId = callData[0].id;
-    const callerId = callData[0].from;
-    
-    // Reject the call
-    await client.rejectCall(callId, callerId);
+  let lastTextTime = 0;
+  const messageDelay = 5000;
 
-    // Check if enough time has passed since the last message
-    const currentTime = Date.now();
-    if (currentTime - lastTextTime >= messageDelay) {
-      // Send the rejection message if the delay has passed
-      await client.sendMessage(callerId, {
-        text: '```❗📵I AM KEITH MD | I REJECT THIS CALL BECAUSE MY OWNER IS BUSY. KINDLY SEND TEXT INSTEAD```.',
-      });
-
-      // Update the last text time
-      lastTextTime = currentTime;
-    } else {
-      console.log('Message skipped to prevent overflow');
-    }
-  }
-});
-
-if (autoreact === 'true') {
-  client.ev.on("messages.upsert", async (chatUpdate) => {
-    try {
-      const mek = chatUpdate.messages[0];  // Assuming 'messages' is an array of messages
-      if (!mek || !mek.message) return;
-
-      const emojiFilePath = path.resolve(__dirname, 'database', 'emojis.json');
-      let emojis = [];
-
-      // Ensure emojis file exists and is valid
-      try {
-        const data = fs.readFileSync(emojiFilePath, 'utf8');
-        emojis = JSON.parse(data);  // Parse the JSON data into an array
-      } catch (error) {
-        console.error('Error reading emojis file:', error);
-        return;
-      }
-
-      // Process the message to react with a random emoji
-      if (!mek.key.fromMe && emojis.length > 0) {
-        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-        await client.sendMessage(mek.key.remoteJid, {
-          react: {
-            text: randomEmoji,
-            key: mek.key,
-          },
+  client.ev.on('call', async (callData) => {
+    if (anticall === 'true') {
+      const callId = callData[0].id;
+      const callerId = callData[0].from;
+      await client.rejectCall(callId, callerId);
+      const currentTime = Date.now();
+      if (currentTime - lastTextTime >= messageDelay) {
+        await client.sendMessage(callerId, {
+          text: '```❗📵I AM KEITH MD | I REJECT THIS CALL BECAUSE MY OWNER IS BUSY. KINDLY SEND TEXT INSTEAD```.',
         });
+        lastTextTime = currentTime;
+      } else {
+        console.log('Message skipped to prevent overflow');
       }
-
-    } catch (error) {
-      console.error('Error processing message:', error);
     }
   });
-}
- 
+
+  if (autoreact === 'true') {
+    client.ev.on("messages.upsert", async (chatUpdate) => {
+      try {
+        const mek = chatUpdate.messages[0];
+        if (!mek || !mek.message) return;
+
+        const emojiFilePath = path.resolve(__dirname, 'database', 'emojis.json');
+        let emojis = [];
+
+        try {
+          const data = fs.readFileSync(emojiFilePath, 'utf8');
+          emojis = JSON.parse(data);
+        } catch (error) {
+          console.error('Error reading emojis file:', error);
+          return;
+        }
+
+        if (!mek.key.fromMe && emojis.length > 0) {
+          const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+          await client.sendMessage(mek.key.remoteJid, {
+            react: {
+              text: randomEmoji,
+              key: mek.key,
+            },
+          });
+        }
+
+      } catch (error) {
+        console.error('Error processing message:', error);
+      }
+    });
+  }
+
   if (autobio === 'true') {
     setInterval(() => {
       const date = new Date();
@@ -139,8 +114,9 @@ if (autoreact === 'true') {
 
   client.ev.on("messages.upsert", async (chatUpdate) => {
     try {
-      mek = chatUpdate.messages[0];
+      let mek = chatUpdate.messages[0];
       if (!mek.message) return;
+
       mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
 
       if (autoview === 'true' && autolike === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") {
@@ -167,8 +143,6 @@ if (autoreact === 'true') {
         }
       }
 
-      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
-
       m = smsg(client, mek, store);
       require("./keith")(client, m, chatUpdate, store);
     } catch (err) {
@@ -176,20 +150,6 @@ if (autoreact === 'true') {
     }
   });
 
-  // Handle error
-  const unhandledRejections = new Map();
-  process.on("unhandledRejection", (reason, promise) => {
-    unhandledRejections.set(promise, reason);
-    console.log("Unhandled Rejection at:", promise, "reason:", reason);
-  });
-  process.on("rejectionHandled", (promise) => {
-    unhandledRejections.delete(promise);
-  });
-  process.on("Something went wrong", function (err) {
-    console.log("Caught exception: ", err);
-  });
-
-  // Setting
   client.decodeJid = (jid) => {
     if (!jid) return jid;
     if (/:\d+@/gi.test(jid)) {
@@ -222,7 +182,6 @@ if (autoreact === 'true') {
   };
 
   client.public = true;
-
   client.serializeM = (m) => smsg(client, m, store);
 
   client.ev.on("group-participants.update", async (m) => {
@@ -259,113 +218,22 @@ if (autoreact === 'true') {
         startKeith();
       }
     } else if (connection === "open") {
-
       await client.groupAcceptInvite("KVkQtTxS6JA0Jctdsu5Tj9");
-
-      console.log(`✅ Connection successful\nLoaded ${totalCommands} commands.\nBot is active.`);
-
-      const getGreeting = () => {
-        const currentHour = DateTime.now().setZone('Africa/Nairobi').hour;
-
-        if (currentHour >= 5 && currentHour < 12) {
-          return 'Good morning 🌄';
-        } else if (currentHour >= 12 && currentHour < 18) {
-          return 'Good afternoon ☀️';
-        } else if (currentHour >= 18 && currentHour < 22) {
-          return 'Good evening 🌆';
-        } else {
-              return 'Good night 😴';
-            }
-        };
-
-
-        const getCurrentTimeInNairobi = () => {
-            return DateTime.now().setZone('Africa/Nairobi').toLocaleString(DateTime.TIME_SIMPLE);
-        };
-
-        let message = `Holla, ${getGreeting()},\n\n╭═══『𝐊𝐞𝐢𝐭𝐡 𝐌𝐝 𝐢𝐬 𝐜𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝』══⊷ \n`;
-
-        message += `║ ʙᴏᴛ ɴᴀᴍᴇ ${botname}\n`;
-message += `║ ᴍᴏᴅᴇ ${mode}\n`;
-message += `║ ᴘʀᴇғɪx [  ${prefix} ]\n`;
-
-message += `║ ᴛᴏᴛᴀʟ ᴘʟᴜɢɪɴs ${totalCommands}\n`
-        message += '║ ᴛɪᴍᴇ ' + getCurrentTimeInNairobi() + '\n';
-        message += '║ ʟɪʙʀᴀʀʏ Baileys\n';
-
-message += `╰═════════════════⊷`
-
-
-
-
-            await client.sendMessage(client.user.id, { text: message });
-        }
-
-
-
-
+      console.log(`✅ Connection successful\nLoaded commands.\nBot is active.`);
+    }
   });
 
   client.ev.on("creds.update", saveCreds);
 
+  app.use(express.static('public'));
 
-  client.sendText = (jid, text, quoted = "", options) => client.sendMessage(jid, { text: text, ...options }, { quoted });
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-    client.downloadMediaMessage = async (message) => { 
-         let mime = (message.msg || message).mimetype || ''; 
-         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]; 
-         const stream = await downloadContentFromMessage(message, messageType); 
-         let buffer = Buffer.from([]); 
-         for await(const chunk of stream) { 
-             buffer = Buffer.concat([buffer, chunk]) 
-         } 
+  app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 
-         return buffer 
-      }; 
-
-
-       
-
- client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => { 
-         let quoted = message.msg ? message.msg : message; 
-         let mime = (message.msg || message).mimetype || ''; 
-         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]; 
-         const stream = await downloadContentFromMessage(quoted, messageType); 
-         let buffer = Buffer.from([]); 
-         for await(const chunk of stream) { 
-             buffer = Buffer.concat([buffer, chunk]); 
-         } 
-         let type = await FileType.fromBuffer(buffer); 
-         const trueFileName = attachExtension ? (filename + '.' + type.ext) : filename; 
-         // save to file 
-         await fs.writeFileSync(trueFileName, buffer); 
-         return trueFileName; 
-     };
-
+  module.exports = startKeith;
 }
 
-
-
-
-
-app.use(express.static('public'));
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/index.html'); 
-});
-
-
-app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
-
 startKeith();
-
-
-module.exports = startKeith;
-
-let file = require.resolve(__filename);
-fs.watchFile(file, () => {
-  fs.unwatchFile(file);
-  console.log(chalk.redBright(`Update ${__filename}`));
-  delete require.cache[file];
-  require(file);
-});
